2e4a2ed0c51b873e80633d0b643990a66481fb3f
三城聖 <sei.e5hayabusa@gmail.com>
2016-09-25 12:15:58 +0900

    first commit

--- Count0925/ViewController.swift
+++ Count0925/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var num: Int = 0
+    @IBOutlet var label:UILabel!
+    @IBOutlet var checkLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,49 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func plus() {
+         num = num + 1
+        label.text = String(num)
+        
+        if num >= 10 {
+            label.textColor = UIColor.redColor()
+        }else if num <= -10 {
+            label.textColor = UIColor.blueColor()
+        }else{
+            label.textColor = UIColor.yellowColor()
+        }
+        
+       checkLabel.text = hantei(num)
+    }
+    
+    @IBAction func minus() {
+        num = num - 1
+        label.text = String(num)
+        
+        if num >= 10 {
+            label.textColor = UIColor.redColor()
+        }else if num <= -10 {
+            label.textColor = UIColor.blueColor()
+        }else{
+            label.textColor = UIColor.yellowColor()
+        }
+        
+        checkLabel.text = hantei(num)
+    }
+    
+    func hantei(x:Int) -> String {
+        if x > 0 {
+            return "プラス"
+        }else if x < 0 {
+            return "マイナス"
+        }else {
+            return "ゼロ"
+        }
+    }
+    
+    
+    
 
 }
 
